8dcd33342b1da47635b1d8cd3f9cc4baa6745913
lixifun <lixifun@qq.com>
2022-06-15 16:00:39 +0800

    update

---
+++ update_yd.bat
@@ -0,0 +1,3 @@
+@echo off
+setproxy && wget https://youtube-dl.org/downloads/latest/youtube-dl.exe -O youtube-dl.exe
+@echo on
--- yd.bat
+++ yd.bat
@@ -1,1 +1,3 @@
+@echo off
 start youtube-dl --proxy http://127.0.0.1:1081 -f best %*
+@echo on
